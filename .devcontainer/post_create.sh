0de236077b70bfed0db13b3600964f21f7e21135
philgweber <philweber@microsoft.com>
2026-03-03 12:17:19 -0800

    Make changes for devcontainer and Makefiles
    Move patina-qemu to subfolder and clean up Makefile deps and devcontainer.

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,15 +1,8 @@
 #!/bin/bash +x
 
-# Give ownership to vscode for python, cargo and rusrtup caches
-sudo chown -R vscode:vscode /home/vscode/.cache
-sudo chown -R vscode:vscode /home/vscode/.cargo
-sudo chown -R vscode:vscode /home/vscode/.rustup
-
-pip install --upgrade -r bios/pip-requirements.txt
-rustup default stable
-rustup target add aarch64-unknown-none
-rustup component add llvm-tools-preview
-cargo install cargo-binutils
+# Give ownership to vscode for installing modules
+sudo chown -R vscode:vscode /home/vscode
+pip install --upgrade -r bios/patina-qemu/pip-requirements.txt
 
 # shellcheck disable=SC2016
 echo 'eval "$(fzf --bash)"' >> ~/.bashrc
