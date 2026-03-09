6d32987af2b1945949264353c2355f2726c784dd
Ron Gurr <54549321+rogurr@users.noreply.github.com>
2026-03-09 16:21:31 -0700

    Update post_create.sh to change ownership of /opt/venv
    Add ownership change for virtual environment directory

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,6 +2,7 @@
 
 # Give ownership to vscode for installing modules
 sudo chown -R vscode:vscode /home/vscode
+sudo chown -R vscode:vscode /opt/venv
 pip install --upgrade -r bios/patina-qemu/pip-requirements.txt
 
 # shellcheck disable=SC2016
